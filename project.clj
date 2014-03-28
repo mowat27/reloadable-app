ca53d7ca8a1c95c145ab2a486703cd4d534c8c27
Adrian Mowat <adrian.mowat@arnoldclark.co.uk>
2014-03-28 17:05:21 +0000

    Set release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloadable-app/lein-template "0.1.0-SNAPSHOT"
+(defproject reloadable-app/lein-template "0.1.0"
   :description "Leiningen template for a new component based app implementing the reloaded workflow."
   :url "https://github.com/mowat27/reloadable-app"
   :license {:name "Eclipse Public License"
