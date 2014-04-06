4a91551ed2a35a736c11eb0ee32e55c9b8fd0b07
Adrian Mowat <adrian.mowat@gmail.com>
2014-04-06 13:47:56 +0100

    Bumped version again
    Fixed a problem with GPG configuration so bumped version to get
    a clean release to Clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloadable-app/lein-template "0.1.1"
+(defproject reloadable-app/lein-template "0.1.2"
   :description "Leiningen template for a new component based app implementing the reloaded workflow."
   :url "https://github.com/mowat27/reloadable-app"
   :license {:name "Eclipse Public License"
